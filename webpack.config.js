9411b4a22f1da02d7b77c7dd0d35a8da6419f4d1
drrckchng <19944575+drrckchng@users.noreply.github.com>
2024-01-21 23:39:32 +0900

    Add entry

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,7 @@ module.exports = {
   mode: 'development',
   entry: {
     index: './src/index.js',
+    getHistoricTemp: './src/getHistoricTemp.js',
   },
   output: {
     filename: '[name].bundle.js',
