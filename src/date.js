549cb7c66d7a9b3b9e177a699b435e6f376b0372
drrckchng <19944575+drrckchng@users.noreply.github.com>
2024-01-31 18:01:26 +0900

    Gather more dates

--- src/date.js
+++ src/date.js
@@ -3,7 +3,10 @@ import { format, sub } from 'date-fns';
 // Return array of dates formatted as strings
 export function getDates() {
   const today = format(new Date(), "yyyy-MM-dd");
-  const years = [1, 5, 10, 20, 30, 40, 50, 60, 70, 80];
+  const years =
+    [1, 5, 10, 15, 20, 25,
+    30, 35, 40, 45, 50, 55,
+    60, 65, 70, 75, 80];
   const dates = [];
   for(let i = 0; i < years.length; i++) {
     const newDate = format(sub(today, {
